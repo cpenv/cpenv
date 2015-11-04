c5556f69982525eb3634d6a10614770083c548e1
Dan Bradham <danielbradham@gmail.com>
2015-11-04 13:07:47 -0500

    0.1.10

--- cpenv/__init__.py
+++ cpenv/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 __title__ = 'cpenv'
-__version__ = '0.1.9'
+__version__ = '0.1.10'
 __author__ = 'Dan Bradham'
 __email__ = 'danielbradham@gmail.com'
 __url__ = 'http://github.com/cpenv/cpenv'
