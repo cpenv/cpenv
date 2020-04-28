4e2b03dc681599aa09a4de8fc5fcc2afa16d04a0
Dan Bradham <danielbradham@gmail.com>
2020-04-28 15:35:09 -0400

    add: versions module to parse module versions

---
+++ cpenv/versions.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+
+# Standard library imports
+import re
+from collections import namedtuple
+from functools import total_ordering
+
+__all__ = [
+    'ParseError',
+    'Version',
+    'parse',
+]
+
+
+semcal_version_pattern = (
+    r'(?:v)?'
+    r'(?P<major>\d+)'
+    r'\.(?P<minor>\d+)'
+    r'\.(?P<patch>\d+)'
+    r'(?:-(?P<prerelease>(?:\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)'
+    r'(?:\.(?:\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?'
+    r'(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$'
+)
+simple_version_pattern = r'(?:v)?(?P<version>(\d+\.?)+)$'
+VersionBase = namedtuple(
+    'Version',
+    ['major', 'minor', 'patch', 'prerelease', 'buildmetadata', 'string']
+)
+
+
+@total_ordering
+class Version(VersionBase):
+
+    _defaults = {
+        'major': 0,
+        'minor': 0,
+        'patch': 0,
+        'prerelease': None,
+        'buildmetadata': None
+    }
+
+    def _cmp(self):
+        return (
+            int(self.major),
+            int(self.minor),
+            int(self.patch),
+            self.prerelease or 'zzzzzzzz',
+            self.buildmetadata or 'zzzzzzzz',
+        )
+
+    def __lt__(self, other):
+        if not isinstance(other, Version):
+            raise ValueError('Can only compare two Version objects.')
+
+        return self._cmp() < other._cmp()
+
+    def __eq__(self, other):
+        if not isinstance(other, Version):
+            raise ValueError('Can only compare two Version objects.')
+
+        return self._cmp() == other._cmp()
+
+
+class ParseError(Exception):
+    '''Raised when a parse method fails.'''
+
+
+def parse(string):
+    '''Attempt to parse a version from the provided string.
+
+    Supports:
+      - semver / calver
+      - simple versions like: 10, v2, 1.0, 2.2.4
+
+    Arguments:
+        string (str): String to parse version from.
+
+    Returns:
+        Version object
+
+    Raises:
+        ParseError when a version can not be parsed.
+    '''
+
+    # Parse Semver / Calver
+    match = re.search(semcal_version_pattern, string)
+    if match:
+        return Version(
+            major=match.group('major'),
+            minor=match.group('minor'),
+            patch=match.group('patch'),
+            prerelease=match.group('prerelease'),
+            buildmetadata=match.group('buildmetadata'),
+            string=match.group(0),
+        )
+
+    # Parse Simple version
+    match = re.search(simple_version_pattern, string)
+    if match:
+        kwargs = dict(Version._defaults)
+        kwargs['string'] = match.group(0)
+        version_parts = match.group('version').split('.')
+        for part, part_name in zip(version_parts, ['major', 'minor', 'patch']):
+            kwargs[part_name] = part
+        return Version(**kwargs)
+
+    raise ParseError('Could not parse version from %s' % string)
---
+++ tests/test_versions.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+
+# Third party imports
+from nose.tools import assert_raises
+
+# Local imports
+from cpenv import versions
+
+
+def test_parse_version():
+    '''Parse semver, calver, and simple version strings'''
+
+    # These should be parsed by parsers.semver_version_pattern
+    basic_semver = [
+        versions.parse('0.1.0'),
+        versions.parse('v0.1.0'),
+        versions.parse('module0.1.0'),
+        versions.parse('module-0.1.0'),
+        versions.parse('module_0.1.0'),
+        versions.parse('modulev0.1.0'),
+        versions.parse('module-v0.1.0'),
+        versions.parse('module_v0.1.0'),
+        versions.parse('module2-0.1.0'),
+    ]
+    for version in basic_semver:
+        assert version.major == '0'
+        assert version.minor == '1'
+        assert version.patch == '0'
+        assert not version.prerelease
+        assert not version.buildmetadata
+
+    full_semver_spec = [
+        versions.parse('10.2.19-dev+02ab'),
+        versions.parse('v10.2.19-dev+02ab'),
+        versions.parse('module10.2.19-dev+02ab'),
+        versions.parse('module-10.2.19-dev+02ab'),
+        versions.parse('module_10.2.19-dev+02ab'),
+        versions.parse('modulev10.2.19-dev+02ab'),
+        versions.parse('module-v10.2.19-dev+02ab'),
+        versions.parse('module_v10.2.19-dev+02ab'),
+        versions.parse('module1-10.2.19-dev+02ab'),
+    ]
+
+    for version in full_semver_spec:
+        assert version.major == '10'
+        assert version.minor == '2'
+        assert version.patch == '19'
+        assert version.prerelease == 'dev'
+        assert version.buildmetadata == '02ab'
+
+    # These should be parsed by versions.simple_version_pattern
+    version = versions.parse('module1')
+    assert version.string == '1'
+    assert version.major == '1'
+    assert not version.minor
+    assert not version.patch
+
+    version = versions.parse('module-1')
+    assert version.string == '1'
+    assert version.major == '1'
+    assert not version.minor
+    assert not version.patch
+
+    version = versions.parse('module-v20')
+    assert version.string == 'v20'
+    assert version.major == '20'
+    assert not version.minor
+    assert not version.patch
+
+    version = versions.parse('modulev1.0')
+    assert version.string == 'v1.0'
+    assert version.major == '1'
+    assert version.minor == '0'
+    assert not version.patch
+
+
+def test_parse_version_raises():
+    '''Parse version raises ParseError when parse fails'''
+
+    bad_strings = [
+        'mod10funk',
+        'no-v',
+        '10-0_module',
+        'module',
+    ]
+
+    for string in bad_strings:
+        assert_raises(versions.ParseError, versions.parse, string)
+
+
+def test_compare_versions():
+    '''Validate Version comparisons'''
+
+    version_strings = [
+        '10.1.5-alpha',
+        '10.1.5',
+        '3.2',
+        '0.1.1',
+        '10.20.5',
+        '2.0',
+        '0.1.0',
+        '3.1.9',
+    ]
+    expected_order = [
+        '0.1.0',
+        '0.1.1',
+        '2.0',
+        '3.1.9',
+        '3.2',
+        '10.1.5-alpha',
+        '10.1.5',
+        '10.20.5',
+    ]
+
+    # Parse and sort
+    version_objects = [versions.parse(v) for v in version_strings]
+    ordered_versions = [v.string for v in sorted(version_objects)]
+    assert ordered_versions == expected_order
