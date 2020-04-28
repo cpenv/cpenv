34f3c7df047263b2bc489027fea290497b61ee01
Dan Bradham <danielbradham@gmail.com>
2020-04-28 15:41:41 -0400

    change: clean up shell.py

--- cpenv/shell.py
+++ cpenv/shell.py
@@ -20,11 +20,11 @@ def run(*args, **kwargs):
         subprocess.check_call(' '.join(args), **kwargs)
         return True
     except subprocess.CalledProcessError:
-        logger.debug('Error running: {}'.format(args))
+        print('Error running: {}'.format(args))
         return False
 
 
-def cmd():
+def get_subshell_command():
     '''Return a command to launch a subshell'''
 
     if platform == 'win':
@@ -35,18 +35,18 @@ def cmd():
         ppid_cmdline_file = '/proc/{0}/cmdline'.format(ppid)
         try:
             with open(ppid_cmdline_file) as f:
-                cmd = f.read()
-            if cmd.endswith('\x00'):
-                cmd = cmd[:-1]
-            cmd = cmd.split('\x00')
-            return cmd + [binpath('subshell.sh')]
+                command = f.read()
+            if command.endswith('\x00'):
+                command = command[:-1]
+            command = command.split('\x00')
+            return command + [binpath('subshell.sh')]
         except Exception:
-            cmd = 'bash'
+            command = 'bash'
 
     else:
-        cmd = 'bash'
+        command = 'bash'
 
-    return [cmd, binpath('subshell.sh')]
+    return [command, binpath('subshell.sh')]
 
 
 def prompt(prefix='', colored=True):
@@ -57,15 +57,15 @@ def prompt(prefix='', colored=True):
     '''
 
     if platform == 'win':
-        return '[{0}] $P$G'.format(prefix)
+        return '{0} $P$G'.format(prefix)
     else:
         if colored:
             return (
-                '[{0}] '  # White prefix
+                '{0} '  # White prefix
                 '\\[\\033[01;32m\\]\\u@\\h\\[\\033[00m\\] '  # Green user@host
                 '\\[\\033[01;34m\\]\\w $ \\[\\033[00m\\]'  # Blue cwd $
             ).format(prefix)
-        return '[{0}] \\u@\\h \\w $ '.format(prefix)
+        return '{0} \\u@\\h \\w $ '.format(prefix)
 
 
 def launch(prompt_prefix=None):
@@ -74,4 +74,4 @@ def launch(prompt_prefix=None):
     if prompt_prefix is not None:
         os.environ['PROMPT'] = prompt(prompt_prefix)
 
-    subprocess.call(cmd(), env=dict(os.environ))
+    subprocess.call(get_subshell_command(), env=dict(os.environ))
