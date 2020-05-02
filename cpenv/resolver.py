10b42595a4d93d31fb7d3356eeab97032e86d072
Dan Bradham <danielbradham@gmail.com>
2020-05-01 20:55:16 -0400

    change: break up Resolver into multiple classes - Resolver: resolves requirements - Activator: activates Modules - Localizer: downloads Modules to local repos - Copier: Copies Modules from one repo to another

--- cpenv/resolver.py
+++ cpenv/resolver.py
@@ -4,161 +4,231 @@
 import os
 
 # Local imports
-from .repos import Repo
-from .module import Module, is_module
-from .utils import (
-    is_redirecting,
-    join_dicts,
-    normpath,
-    redirect_to_modules,
-    set_env,
-    walk_up,
-)
-
-
-'''
-cpenv.resolver
-==============
-Defines :class:`Resolver` used to resolve cpenv :class:`Module` s.
-'''
+from . import utils
+from .repos import Repo, LocalRepo
+from .module import Module, is_module, is_exact_match, best_match
 
 
 class ResolveError(Exception):
-    pass
+    '''Raised when a Resolver fairs to resolve a module or list of modules.'''
 
 
 class Resolver(object):
-    '''Resolve, combine, and activate :class:`Module`s.
+    '''Responsible for resolving ModuleSpecs from requirement strings.
 
-    Modules will be looked up using the resolver functions in the
+    Modules will be looked up sequentially in all registered Repos. By default
+    this order is:
+        1. LocalRepo pointing to current working directory
+        2. LocalRepo pointing to cpenv_home_modules_path ($CPENV_HOME/modules)
+        3. LocalRepo pointing to cpenv_user_modules_path
+        4. LocalRepos pointing to the paths in $CPENV_MODULES
+        5. Custom Repos registered via cpenv.add_repo
+
+    If there are still unresolved modules, we fallback to the old algorithm
+    for module lookups using the resolve functions in the
     cpenv.resolver.module_resolvers list.
-
-    usage::
-
-        >>> Resolver('module_a', 'module_b', 'module_c')
     '''
 
-    def __init__(self, *paths):
+    def __init__(self, repos):
+        self.repos = repos
 
-        if not paths:
-            raise ValueError('Resolver expects at least 1 argument.')
+    def resolve(self, requirements):
+        '''Given a list of requirement strings, resolve ModuleSpecs.
 
-        self.paths = paths
-        self.resolved = None
-        self.modules = None
-        self.combined = None
+        Returns:
+            list of ModuleSpec objects
 
-    def resolve(self):
-        '''Resolve all paths'''
+        Raises:
+            ResolveError when a requirement can not be resolved.
+        '''
 
-        from . import api
+        unresolved = list(requirements)
+        resolved = []
 
-        self.resolved = []
-        paths = list(self.paths)
+        for requirement in requirements:
 
-        # Attempt to resolve paths using Repos
-        for path in list(paths):
+            # TODO: handle more complex requirements.
+            #       possibly use the new resolvelib being developed by pypa
 
-            for repo in api.get_repos():
+            match_gen = (
+                module_spec for repo in self.repos
+                for module_spec in repo.find(requirement)
+            )
 
-                module_or_spec = repo.find_module(path)
-                if module_or_spec:
-                    self.resolved.append(module_or_spec)
-                    paths.remove(path)
+            # best_match returns the first ModuleSpec that matches
+            # both name and version or the ModuleSpec with the
+            # highest version > the requirement
+            match = best_match(requirement, match_gen)
+            if match:
+                unresolved.remove(requirement)
+                resolved.append(match)
+
+        if unresolved:
+            # Try the old resolution alogirthm for backwards compatability
+            resolved.extend(old_resolve_algorithm(self, unresolved))
+
+        if unresolved:
+            raise ResolveError(
+                'Could not resolve: ' + ' '.join(unresolved)
+            )
+
+        return resolved
+
+
+class Activator(object):
+    '''Responsible for activating modules.'''
+
+    def __init__(self, localizer=None):
+        self.localizer = localizer or Localizer(to_repo='home')
+
+    def combine_modules(self, modules):
+        '''Combine a list of module's environments.'''
+
+        return utils.join_dicts(*[obj.environment for obj in modules])
+
+    def activate(self, module_specs):
+        '''Activate a list of module specs.'''
+
+        modules = self.localizer.localize(module_specs)
+        env = self.combine_modules(modules)
+        utils.set_env(env)
+
+        for module in modules:
+            module.activate()
+
+        return modules
+
+
+class Copier(object):
+    '''Responsible for copying modules to a specific module.'''
+
+    def __init__(self, to_repo):
+        from .api import get_repo
+
+        self.to_repo = get_repo(to_repo)
+
+        if not isinstance(self.to_repo, LocalRepo):
+            raise ValueError(
+                'Copier expected LocalRepo got %s' % type(to_repo)
+            )
+
+    def copy(self, module_specs, overwrite=False):
+        '''Given ModuleSpecs, copy them to this copiers to_repo.'''
+        from .api import get_cache_path
+
+        copied = []
+        for module_spec in module_specs:
+
+            matches = self.to_repo.find(module_spec.qual_name)
+            already_exists = False
+            for match in matches:
+                if is_exact_match(module_spec.qual_name, match):
+                    already_exists = True
                     break
 
-        # Call the old resolution algorithm for backwards compatability
-        self.resolved.extend(_old_resolve_algorithm(paths))
-
-        # If we have left over paths we did not resolve everything!!
-        if paths:
-            raise ResolveError(
-                'Could not resolve: ' + ' '.join(paths)
-            )
-
-        return self.resolved
-
-    def localize(self, to_repo='home', overwrite=False):
-        '''Localize all resolved modules.'''
-
-        from . import api
-
-        if not isinstance(to_repo, Repo):
-            to_repo = api.get_repo(name=to_repo)
-
-        if not isinstance(to_repo, LocalRepo):
-            raise ValueError(
-                'to_repo expected LocalRepo got %s' % type(to_repo)
-            )
-
-        if self.resolved is None:
-            raise ValueError('You must call Resolver.resolve first.')
-
-        self.modules = []
-        for source_module in self.resolved:
-            if isinstance(module, Module):
-                self.modules.append(module)
-                continue
-
-            # We hit a ModuleSpec - we need to localize it.
-            module_spec = module
-            from_repo = module.repo
-            existing_module = to_repo.find_module(module_spec.qual_name)
-            if existing_module and not overwrite:
-                self.modules.append(existing_module)
+            if already_exists and not overwrite:
                 continue
 
             # Generate a new module path in to_repo
-            if module.version.string in module.real_name:
-                new_module_path = to_repo.relative_path(module.real_name)
+            download_path = get_cache_path(
+                'temp_downloads',
+                str(hash(module_spec.repo.name)),
+                module_spec.name,
+                module_spec.version.string,
+            )
+            module = module_spec.repo.download(
+                module_spec,
+                where=download_path,
+                overwrite=overwrite,
+            )
+            new_module_spec = self.to_repo.upload(
+                module,
+                overwrite=overwrite
+            )
+            copied.append(new_module_spec)
+
+        temp_downloads = get_cache_path('temp_downloads')
+        if os.path.isdir(temp_downloads):
+            utils.rmtree(temp_downloads)
+
+        return copied
+
+
+class Localizer(object):
+    '''Downloads modules from remote Repos to a specific LocalRepo.
+
+    This is similar to a copy operation, but skips all module_specs that are
+    already in LocalRepos. If they are in LocalRepos then they are already
+    available to be activated.
+    '''
+
+    def __init__(self, to_repo='home'):
+        from .api import get_repo
+
+        self.to_repo = get_repo(to_repo)
+
+        if not isinstance(self.to_repo, LocalRepo):
+            raise ValueError(
+                'Localizer expected LocalRepo got %s' % type(to_repo)
+            )
+
+
+    def localize(self, module_specs, overwrite=False):
+        '''Given ModuleSpecs, download them to this Localizers repo.'''
+
+        localized = []
+        for module_spec in module_specs:
+
+            # Module is already local
+            if isinstance(module_spec.repo, LocalRepo):
+                localized.append(Module(module_spec.path))
+                continue
+
+            # Module already exists in to_repo
+            matches = self.to_repo.find(module_spec.qual_name)
+            already_exists = False
+            for match in matches:
+                if is_exact_match(module_spec.qual_name, match):
+                    already_exists = True
+                    localized.append(Module(match.path))
+                    break
+
+            if already_exists and not overwrite:
+                continue
+
+            # Generate a new module path in to_repo
+            if module_spec.version.string in module_spec.real_name:
+                new_module_path = to_repo.relative_path(module_spec.real_name)
             else:
                 new_module_path = to_repo.relative_path(
-                    module.name,
-                    module.version.string,
+                    module_spec.name,
+                    module_spec.version.string,
                 )
 
-            module = from_repo.clone_module(
-                module=module_spec,
+            module = module_spec.repo.download(
+                module_spec,
                 where=new_module_path,
                 overwrite=overwrite,
             )
-            self.modules.append(module)
+            localized.append(module)
 
-    def combine(self):
-        '''Combine all of the resolved modules environments.'''
-
-        if self.modules is None:
-            raise ValueError('You must call resolve and localize first.')
-
-        return join_dicts(*[obj.environment for obj in self.modules])
-
-    def activate(self):
-        '''Active this resolvers resolved modules.'''
-
-        if self.resolved is None:
-            self.resolve()
-
-        if self.modules is None:
-            self.localize()
-
-        # Combine and set environment variables
-        set_env(self.combine())
-
-        # Activate all modules
-        for obj in self.modules:
-            obj.activate()
+        return localized
 
 
-def _old_resolve_algorithm(paths):
-    '''Extracted and maintained for backwards compatability.'''
+# Deprecated!!
+# The following functions represet the old resolution algorithm
+# this is called by Resolver.resolve only when no Repos are able to resolve
+# a requirement.
+
+def old_resolve_algorithm(resolver, paths):
 
     modules = []
     for path in list(paths):
 
-        for resolver in module_resolvers:
+        for module_resolver in module_resolvers:
             try:
 
-                resolved = resolver(path)
+                resolved = module_resolver(resolver, path)
                 paths.remove(path)
 
                 if isinstance(resolved, Module):
@@ -173,7 +243,7 @@ def _old_resolve_algorithm(paths):
     return modules
 
 
-def path_is_module_resolver(path):
+def path_is_module_resolver(resolver, path):
     '''Checks if path is already a :class:`Module` object'''
 
     if isinstance(path, Module):
@@ -182,31 +252,31 @@ def path_is_module_resolver(path):
     raise ResolveError
 
 
-def cwd_resolver(path):
+def cwd_resolver(resolver, path):
     '''Checks if path is already a :class:`Module` object'''
 
-    mod_path = normpath(os.getcwd(), path)
+    mod_path = utils.normpath(os.getcwd(), path)
     if is_module(mod_path):
-        return Module(mod_path)
+        return Module(mod_path).as_spec()
 
     raise ResolveError
 
 
-def modules_path_resolver(path):
+def modules_path_resolver(resolver, path):
     '''Resolves modules in CPENV_MODULES path and CPENV_HOME/modules'''
 
     from .api import get_module_paths
 
     for module_dir in get_module_paths():
-        mod_path = normpath(module_dir, path)
+        mod_path = utils.normpath(module_dir, path)
 
         if is_module(mod_path):
-            return Module(mod_path)
+            return Module(mod_path).as_spec()
 
     raise ResolveError
 
 
-def redirect_resolver(path):
+def redirect_resolver(resolver, path):
     '''Resolves environment from .cpenv file...recursively walks up the tree
     in attempt to find a .cpenv file'''
 
@@ -216,11 +286,13 @@ def redirect_resolver(path):
     if os.path.isfile(path):
         path = os.path.dirname(path)
 
-    for root, _, _ in walk_up(path):
-        if is_redirecting(root):
-            env_paths = redirect_to_modules(normpath(root, '.cpenv'))
-            r = Resolver(*env_paths)
-            return r.resolve()
+    for root, _, _ in utils.walk_up(path):
+        if utils.is_redirecting(root):
+            env_paths = utils.redirect_to_modules(
+                utils.normpath(root, '.cpenv')
+            )
+            r = Resolver(resolver.repos)
+            return r.resolve(*env_paths)
 
     raise ResolveError
 
