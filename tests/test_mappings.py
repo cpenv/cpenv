5151c288a47e47fc49345f9ed486e457a27469d0
Dan Bradham <danielbradham@gmail.com>
2020-08-20 14:40:54 -0400

    fix: #27 make join_dicts case insensitive add: CaseInsensitiveDict class

--- tests/test_mappings.py
+++ tests/test_mappings.py
@@ -49,5 +49,42 @@ def test_prepend_values():
     assert result['var'] == ['x', 'y', 'z']
 
     result = mappings.join_dicts(a, b, c)
-    print(result)
     assert result['var'] == ['0', '1', 'x', 'y', 'z']
+
+
+def test_join_case_insensitivity():
+    '''join_dicts - is case insensitive.'''
+
+    a = {'Var': 'a'}  # Original mixed case
+    b = {'VAR': 'b'}  # UPPER - set
+    c = {'var': ['0', '1']}  # lower - prepend
+
+    # Ensure Var is properly set and case of key is changed
+    result = mappings.join_dicts(a, b)
+    assert result['VAR'] == 'b'
+
+    # Ensure Var is properly set, prepended to and case of key is changed
+    result = mappings.join_dicts(a, b, c)
+    assert result['var'] == ['0', '1', 'b']
+
+
+def test_env_to_dict():
+    '''env_to_dict - convert environment mapping to dict'''
+
+    env = {
+        'PATH': 'X:Y:Z',
+        'VAR': 'VALUE',
+    }
+    result = mappings.env_to_dict(env, pathsep=':')
+    assert result == {'PATH': ['X', 'Y', 'Z'], 'VAR': 'VALUE'}
+
+
+def test_dict_to_env():
+    '''dict_to_env - convert dict to environment mapping'''
+
+    data = {
+        'PATH': ['X', 'Y', 'Z'],
+        'VAR': 'VALUE',
+    }
+    result = mappings.dict_to_env(data, pathsep=':')
+    assert result == {'PATH': 'X:Y:Z', 'VAR': 'VALUE'}
