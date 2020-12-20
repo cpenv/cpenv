da958d42fca78e8de613b9941f250ea1ac23d294
Dan Bradham <danielbradham@gmail.com>
2020-12-20 14:46:03 -0500

    change: bump version to 0.5.15

--- cpenv/__init__.py
+++ cpenv/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 '''isort:skip_file'''
 __title__ = 'cpenv'
-__version__ = '0.5.14'
+__version__ = '0.5.15'
 __author__ = 'Dan Bradham'
 __email__ = 'danielbradham@gmail.com'
 __url__ = 'http://github.com/cpenv/cpenv'
